c5535d60a4235238d7a78212ae56b001db5f9cf7
Linda Sunnergård <lisu21@student.bth.se>
2024-01-08 10:44:56 +0100

    added docker push

--- index.ts
+++ index.ts
@@ -12,6 +12,7 @@ const token = ScooterApi.token(scooterId).then((value) => {
 const updateTime = Number(ScooterApi.getEnvVariable("HARDWARE_UPDATE"))
 
 // TODO: Lägga till volymen i bash-scriptet: "docker run -d -e SCOOTERID=$counter -e PASSWORD=$counter --network=... --link=... --port=... -volume="hardware:/scooter-app/model/hardware/:ro" <image>"
+// TODO: Tester får inte till token env???
 
 /**
  * Websocket client.
